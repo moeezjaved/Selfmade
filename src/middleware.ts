9dee2ff4ec39c2ef5f2a6d500f4fc26b8f8c6e50
Moeez Javed <hamzaharron@Hamzas-MacBook-Pro.local>
2026-04-24 19:57:51 +0500

    fix middleware type error

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         getAll() { return request.cookies.getAll() },
-        setAll(cookies) {
+        setAll(cookies: { name: string; value: string; options?: any }[]) {
           cookies.forEach(({ name, value }) => request.cookies.set(name, value))
           response = NextResponse.next({ request })
           cookies.forEach(({ name, value, options }) => response.cookies.set(name, value, options))
